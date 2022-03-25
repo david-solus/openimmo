29a47f85bbda5f39ba9448527f0470ad4e3db115
David Solus <david.marquardt@outlook.de>
2022-03-25 12:20:24 +0100

    inserted empty space on "flaeche"

--- src/API/Flaechen.php
+++ src/API/Flaechen.php
@@ -311,6 +311,16 @@ class Flaechen
     protected $userDefinedExtend;
 
     /**
+     * This value is needed because a class with only xml attributes
+     * will result in using any next value as an xml value of this tag
+     *
+     * @Type("string")
+     * @XmlValue
+     * @var string
+     */
+    private $xmlEmptySpace = " ";
+    
+    /**
      * @return float
      */
     public function getAnzahlBadezimmer(): ?float
